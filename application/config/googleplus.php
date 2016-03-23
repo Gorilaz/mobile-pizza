8d3fb6180441d4eb93fb96815fd0d7075636db87
Dmitry <djrebus@gmail.com>
2016-03-23 16:21:30 +0400

    Debug for G+ part 1

--- application/config/googleplus.php
+++ application/config/googleplus.php
@@ -1,8 +1,8 @@
 <?php
 
     $config['googleplus']['application_name'] = 'Bluestar Pizza';
-    $config['googleplus']['client_id'] = '';
-    $config['googleplus']['client_secret'] = '';
+    $config['googleplus']['client_id'] = '931027829790-08gq4343ecm82mt2eqppmvj1d30c5p30.apps.googleusercontent.com';
+    $config['googleplus']['client_secret'] = 'ufYpl6dvNt0c-i5ixa_tIJBn';
     $config['googleplus']['redirect_uri'] = 'http://bluestar.dmitry.deva/security/googleplus_page';
     $config['googleplus']['api_key'] = 'AIzaSyAQenY_dskVhakdA6Dq6YkYe4yuh8725uk';
 

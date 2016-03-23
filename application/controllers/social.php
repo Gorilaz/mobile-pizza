cee53e84fa0dfc0bee4b9ed61932c156d62ba785
Dmitry <djrebus@gmail.com>
2016-03-23 17:52:47 +0400

    Debug G+ part 2

--- application/controllers/social.php
+++ application/controllers/social.php
@@ -6,7 +6,11 @@ class Social extends WMDS_Controller {
 	{
             header('Content-Type: application/javascript');
             $appId = $this->db->select('value')->where('type', 'facebook_app_id')->get('sitesetting')->row();
+            $appKey = $this->db->select('value')->where('type', 'google_api_key')->get('sitesetting')->row();
+            $clientId = $this->db->select('value')->where('type', 'google_client_id')->get('sitesetting')->row();
             $this->twiggy->set('appId', $appId->value );
+            $this->twiggy->set('appkey', $appKey->value );
+            $this->twiggy->set('clientId', $clientId->value );
             $this->twiggy->template('social-ids')->display();
 	}
 }
--- assets/js/account/login.js
+++ assets/js/account/login.js
@@ -35,63 +35,91 @@ $(function(){
         authFBUser();
     });
     $('#googleLoginButton').click(function(){
-        document.location.href = '/security/googleplus_page';
+        goolePluslogin();
     });
 });
 
 function authFBUser() {
-
     FB.login(checkLoginStatus, {scope:'email'});
-
 }
 
-
 // Check the result of the user status and display login button if necessary
-
 function checkLoginStatus(response) {
     if(response && response.status == 'connected') {
         FB.api('/me?fields=id,name,first_name,last_name,email,location', function(me) {
             $.ajax({
-
                 url: '//' + location.host + '/security/facebook_login',
-
                 data: me,
-
                 type: "POST",
-
                 success: function(result) {
                     window.location.href = '//' + location.host + '/menu';
-
                 },
-
                 error: function(e){
-
                     console.error(e);
-
                 }
-
             });
-
         });
-
-
-
         // Hide the login button
-
 //        document.getElementById('loginButton').style.display = 'none';
-
     } else {
-
 //        alert('User is not authorized');
-
-
-
         // Display the login button
-
 //        document.getElementById('loginButton').style.display = 'block';
-
     }
-
 }
 
 /** By default country is Australia - get states */
+
+/** Google Plus --------------------------------------------------- **/
+function loginCallback(result)
+{
+    if(result['status']['signed_in'])
+    {
+        var request = gapi.client.plus.people.get(
+        {
+            'userId': 'me'
+        });
+        request.execute(function (resp)
+        {
+           console.log(resp);
+           alert('stop');
+        });
+     }
+}
+
+/**
+ * Need to check is present global vars
+ * @type String|GOOGLEPLUSAPPKEY
+ */
+var googleappkey = '';
+if( typeof GOOGLEPLUSAPPKEY == 'string' )
+{
+    googleappkey = GOOGLEPLUSAPPKEY;
+    googleclientid = GOOGLEPLUSCLIENTID;
+}
+var googleclientid = '';
+if( typeof GOOGLEPLUSCLIENTID == 'string' )
+{
+    googleclientid = GOOGLEPLUSCLIENTID;
+}
+/* use after for GooglePlus - var googleappkey and googleclientid */
+
+function goolePluslogin()
+{
+    var myParams = {
+        'clientid' : googleclientid,
+        'cookiepolicy' : 'http://mobile.bluestarpizza.com.au',
+        'callback' : 'loginCallback',
+        'approvalprompt':'force',
+        'scope' : 'https://www.googleapis.com/auth/plus.login https://www.googleapis.com/auth/plus.profile.emails.read'
+    };
+    gapi.auth.signIn(myParams);
+}
+
+function onLoadCallback()
+{
+    gapi.client.setApiKey(googleappkey);
+    gapi.client.load('plus', 'v1', function(){});
+}
+
+/** --------------------------------------------------------------- **/

8f4316d13085b5d788fbaa8509ccd9960988ffe6
Dmitry <djrebus@gmail.com>
2016-04-21 12:20:08 +0400

    DB migrations

---
+++ .migrations/social-ids.sql
@@ -0,0 +1,6 @@
+
+insert into `sitesetting`(`id`,`type`,`value`) values ( NULL,'facebook_app_id','180510192331647');
+insert into `sitesetting`(`id`,`type`,`value`) values ( NULL,'facebook_secret','793b0531b759139b98a01d5d77fc535f');
+insert into `sitesetting`(`id`,`type`,`value`) values ( NULL,'google_api_key','AIzaSyAQenY_dskVhakdA6Dq6YkYe4yuh8725uk');
+insert into `sitesetting`(`id`,`type`,`value`) values ( NULL,'google_client_id','931027829790-08gq4343ecm82mt2eqppmvj1d30c5p30.apps.googleusercontent.com');
+insert into `sitesetting`(`id`,`type`,`value`) values ( NULL,'google_secret','ufYpl6dvNt0c-i5ixa_tIJBn');
